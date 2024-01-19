ce6db6dfba61850a2708581269ca8b7db4d5f4a1
venkatesh <136556477+venkatesh209@users.noreply.github.com>
2024-01-19 11:23:08 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.scss';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage';
+import Banner from "./components/Banner/Banner";
+import Header from './components/Header/Header';
+
 
 function App() {
   return (
     <> 
       <Router>
-        <Switch>
-          <Route path="/" exact component={HomePage}/>
-        </Switch>
+      <Header />
+      <Banner/>
       </Router>
     </>
   );
